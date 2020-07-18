4583372f88fec8391fa82abd262bad17d9412cd9
NSStudent <omarmn83@gmail.com>
2020-07-18 21:36:10 +0200

    wip

---
+++ Sources/ValorantComponents/BalorantButtonStyle.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  
+//
+//  Created by Omar Megdadi on 18/07/2020.
+//
+
+import SwiftUI
+
+public struct RounderButtonStyle: ButtonStyle {
+    public func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .foregroundColor(.white)
+            .padding()
+            .background(Color.VABackground)
+            
+    }
+}
